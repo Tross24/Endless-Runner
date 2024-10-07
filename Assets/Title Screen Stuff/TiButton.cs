0f478baa2a632c3e345ab868dde6ea758c7cfe6d
Tross24 <tross2024@elam.co.uk>
2024-10-07 15:40:12 +0100

    Fish Guide Implement Update

--- Assets/Title Screen Stuff/TiButton.cs
+++ Assets/Title Screen Stuff/TiButton.cs
@@ -18,34 +18,6 @@ public class TiButton : MonoBehaviour
 
         //Takes you to title
     }
-    public void Night()
-    {
-        SceneManager.LoadSceneAsync(3);
-        Time.timeScale = 1;
-
-        //Takes you to title
-    }
-    public void Sunset()
-    {
-        SceneManager.LoadSceneAsync(9);
-        Time.timeScale = 1;
-
-        //Takes you to title
-    }
-    public void Storm()
-    {
-        SceneManager.LoadSceneAsync(4);
-        Time.timeScale = 1;
-
-        //Takes you to title
-    }
-    public void Day()
-    {
-        SceneManager.LoadSceneAsync(5);
-        Time.timeScale = 1;
-
-        //Takes you to title
-    }
     public void Options()
     {
         SceneManager.LoadSceneAsync(2);
@@ -53,6 +25,67 @@ public class TiButton : MonoBehaviour
         Debug.Log(Time.timeScale);
         //Takes you to title
     }
+    public void FGpg1()
+    {
+        SceneManager.LoadSceneAsync(3);
+        Time.timeScale = 1;
 
-    
+        //Takes you to title
+    }
+    public void FGpg2()
+    {
+        SceneManager.LoadSceneAsync(4);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg3()
+    {
+        SceneManager.LoadSceneAsync(5);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg4()
+    {
+        SceneManager.LoadSceneAsync(6);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg5()
+    {
+        SceneManager.LoadSceneAsync(7);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg6()
+    {
+        SceneManager.LoadSceneAsync(8);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg7()
+    {
+        SceneManager.LoadSceneAsync(9);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void FGpg8()
+    {
+        SceneManager.LoadSceneAsync(10);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
+    public void Night()
+    {
+        SceneManager.LoadSceneAsync(11);
+        Time.timeScale = 1;
+
+        //Takes you to title
+    }
 }
